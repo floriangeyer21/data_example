e3e21a4a6a38e1cd1aefe4ae65f299bc9a7d3837
vladd <vladdyachkin11@gmail.com>
2020-10-17 13:42:27 +0300

    first commit

---
+++ src/main/java/com/data/example/data_example/DataExampleApplication.java
@@ -0,0 +1,13 @@
+package com.data.example.data_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/data/example/data_example/DataExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.example.data_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
